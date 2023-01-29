2756790daf9c9e2a8ede83de7302d98dd9fbfe37
pAsta-kun <112457969+pAsta-kun@users.noreply.github.com>
2023-01-28 20:42:40 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let backgrounds = ["url('images/streetWithPeople.jpeg')", "url('images/topViewPeople.jpeg')", ];
+let counter = 0;
+let backgroundChangeTimer;
+let headerBackground = document.getElementById('headerBackground');
+function backgroundChange()
+{
+    backgroundChangeTimer = setInterval(function()
+    {
+        counter++;
+        if(counter > 1)
+            counter = 0;
+        headerBackground.style.backgroundImage = backgrounds[counter];
+        headerBackground.style.transition = "500ms ease-in"
+        console.log(counter);
+    }, 5000);
+    
+}
